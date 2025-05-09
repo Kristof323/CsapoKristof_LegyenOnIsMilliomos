2116e59b8bd855b925c421d40e0f8150975c37f0
Csapó Kristóf Zoltán <kristofcsapo@gmail.com>
2025-05-09 10:59:12 +0200

    Add project files.

---
+++ CsapoKristof_LegyenOnIsMilliomos/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CsapoKristof_LegyenOnIsMilliomos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

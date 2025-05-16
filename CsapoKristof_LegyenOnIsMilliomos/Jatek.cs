a59c593a715b2129e70fe5a676d32457db41cfe4
Csapó Kristóf Zoltán <kristofcsapo@gmail.com>
2025-05-16 11:39:37 +0200

    kezdet

---
+++ CsapoKristof_LegyenOnIsMilliomos/Jatek.cs
@@ -0,0 +1,58 @@
+﻿public class Jatek
+{
+    private Kerdesek kerdesek;
+    private int aktualisSzint = 1;
+    private int nyertOsszeg = 0;
+    private Dictionary<string, bool> segitsegek = new Dictionary<string, bool>
+    {
+        {"kozonseg", true},
+        {"felezo", true},
+        {"telefon", true}
+    };
+
+
+    public Jatek(Kerdesek kerdesek)
+    {
+        this.kerdesek = kerdesek;
+    }
+
+    public void Indit()
+    {
+        aktualisSzint = 1;
+        nyertOsszeg = 0;
+        hasznaltKerdesek.Clear();
+        segitsegek["kozonseg"] = true;
+        segitsegek["felezo"] = true;
+        segitsegek["telefon"] = true;
+
+
+
+    private void HasznalSegitseg(string tipus, Kerdes kerdes)
+    {
+        switch (tipus)
+        {
+            case "felezo":
+
+                Console.WriteLine("Felező után ezek maradtak:");
+                foreach (var betu in new[] { "A", "B", "C", "D" })
+                {
+                    if (!torlendo.Contains(betu))
+                    {
+                        int index = Array.IndexOf(new[] { "A", "B", "C", "D" }, betu);
+                        Console.WriteLine($"{betu}: {kerdes.Valaszok[index]}");
+                    }
+                }
+                break;
+
+            case "kozonseg":
+                Console.WriteLine($"A közönség szerint a helyes válasz: {kerdes.HelyesValasz} (80%)");
+                break;
+
+            case "telefon":
+                Console.WriteLine($"A telefonos barát szerint a helyes válasz: {kerdes.HelyesValasz}");
+                break;
+        }
+
+        segitsegek[tipus] = false;
+    }
+}
--- CsapoKristof_LegyenOnIsMilliomos/Program.cs
+++ CsapoKristof_LegyenOnIsMilliomos/Program.cs
@@ -1,10 +1,11 @@
-﻿namespace CsapoKristof_LegyenOnIsMilliomos
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace CsapoKristof_LegyenOnIsMilliomos
 {
     internal class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+
     }
-}

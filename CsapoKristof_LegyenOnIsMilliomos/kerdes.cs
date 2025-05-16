a16af6fafa49195c0de5814c003a4998202ee517
Csapó Kristóf Zoltán <kristofcsapo@gmail.com>
2025-05-16 11:46:25 +0200

    kerdes.cs kezdet

---
+++ CsapoKristof_LegyenOnIsMilliomos/kerdes.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsapoKristof_LegyenOnIsMilliomos
+{
+    internal class kerdes
+    {
+        public string Szoveg { get; set; }
+        public List<string> Valaszok { get; set; }
+        public char HelyesValasz { get; set; }
+        public string Kategoria { get; set; }
+
+        public Kerdes(string szoveg, List<string> valaszok, char helyesValasz, string kategoria)
+        {
+            Szoveg = szoveg;
+            Valaszok = valaszok;
+            HelyesValasz = helyesValasz;
+            Kategoria = kategoria;
+        }
+    }
+}
